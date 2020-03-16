b3e9a728312adbe6075e29543adda3f84aee16be
Nils Hoffmann <nils.hoffmann@god.de>
2020-03-16 19:59:00 +0100

    Initial Commit

---
+++ src/main/java/dev/nhoffmann/thelibrary/TheLibraryApplication.java
@@ -0,0 +1,13 @@
+package dev.nhoffmann.thelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nhoffmann/thelibrary/TheLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nhoffmann.thelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
